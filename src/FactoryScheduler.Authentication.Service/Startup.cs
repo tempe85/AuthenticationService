2764e9c5602db5cc4da9d50b06f91ab87f8b5a70
Zach Tindell <Zach_Tindell@selinc.com>
2021-08-08 20:36:34 -0700

    Updated connection settings

--- src/FactoryScheduler.Authentication.Service/Startup.cs
+++ src/FactoryScheduler.Authentication.Service/Startup.cs
@@ -85,7 +85,8 @@ namespace FactoryScheduler.Authentication.Service
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            // if (env.IsDevelopment())
+            if (true)
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
